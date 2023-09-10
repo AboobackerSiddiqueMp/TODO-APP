b391cbfda889e84db88cd7464d7d7f8936046277
aboobacker <saboobacker189@gmail.com>
2023-09-11 01:20:25 +0530

    completad

--- js/script.js
+++ js/script.js
@@ -9,7 +9,30 @@ function addTask(){
         let li= document.createElement("li")
         li.innerHTML=inpuBox.value
         listContainer.appendChild(li)
-        inpuBox.value=""
+        let span=document.createElement("span")
+        span.innerHTML="\u00d7";
+        li.appendChild(span)
     }
+    inpuBox.value=""
+    saveData()
+
 
 }
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked")
+        saveData()
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData()
+
+    }
+})
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML)
+}
+function showList(){
+    listContainer.innerHTML=localStorage.getItem("data")
+}
+showList()
